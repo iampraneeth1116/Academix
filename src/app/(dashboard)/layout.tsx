6af2cb9d5cc37ec63724f016b873dfaf89ac5e17
Praneeth <iampraneeth1116@gmail.com>
2025-11-01 17:44:52 +0530

    added navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,8 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
+
 
 export default function DashboardLayout({
   children,
@@ -17,7 +20,13 @@ export default function DashboardLayout({
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block font-bold">Academix</span>
         </Link>
-      </div>  
+        <Menu />
+      </div>
+
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+        <Navbar />
+        {children}
+      </div>
     </div>
   );
 }
