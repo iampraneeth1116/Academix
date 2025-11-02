79eedd9b38e1c1eff07f5652d8fc79ed8071cceb
Praneeth <iampraneeth1116@gmail.com>
2025-11-02 15:14:51 +0530

    added parent page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -8,7 +8,7 @@ import Announcements from "@/components/Announcements";
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* LEFT */}
+      {/* L */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
 
         <div className="flex gap-4 justify-between flex-wrap">
@@ -33,7 +33,7 @@ const AdminPage = () => {
           <FinanceChart />
         </div>
       </div>
-      {/* RIGHT */}
+      {/* R */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
         <Announcements/>
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -5,14 +5,14 @@ import EventCalendar from "@/components/EventCalendar";
 const StudentPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
-      {/* LEFT */}
+      {/* L */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
           <h1 className="text-xl font-semibold">Schedule (4A)</h1>
           <BigCalendar/>
         </div>
       </div>
-      {/* RIGHT */}
+      {/* R */}
       <div className="w-full xl:w-1/3 flex flex-col gap-8">
         <EventCalendar />
         <Announcements />
