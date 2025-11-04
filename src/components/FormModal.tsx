b517a80a957bfc9d6b03844faa059c00f36db43d
Praneeth <iampraneeth1116@gmail.com>
2025-11-04 22:48:35 +0530

    fdone

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -77,7 +77,7 @@ const FormModal = ({
         <Image src={`/${type}.png`} alt="" width={16} height={16} />
       </button>
       {open && (
-        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black/60 z-50 flex items-center justify-center">
           <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
             <Form />
             <div
