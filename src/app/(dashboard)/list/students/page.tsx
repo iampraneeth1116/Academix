ae767f269e3821c6bf21b34c45b64dc8d71a4b99
Praneeth <iampraneeth1116@gmail.com>
2025-11-04 16:17:41 +0530

    updated studentlistpage

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -4,6 +4,8 @@ import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
 import { role, studentsData } from "@/lib/data";
 import Table from "@/components/Table";
+import FormModal from "@/components/FormModal";
+
 
 
 type Student = {
@@ -80,10 +82,7 @@ const StudentListPage = () => {
             </button>
           </Link>
           {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-aPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-
+            <FormModal table="student" type="delete" id={item.id}/>
           )}
         </div>
       </td>
@@ -105,17 +104,13 @@ const StudentListPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-aYellow">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
-
+              <FormModal table="student" type="create"/>
             )}
           </div>
         </div>
       </div>
       {/* LIST */}
       <Table columns={columns} renderRow={renderRow} data={studentsData} />
-
       <Pagination />
     </div>
   );
