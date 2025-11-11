d5f6a330b568dd396519ced143ca23dc7eb1fc56
Praneeth <iampraneeth1116@gmail.com>
2025-11-12 02:44:17 +0530

    Fixed middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -8,24 +8,23 @@ export default function proxy(req: NextRequest) {
   if (
     pathname.startsWith("/_next") ||
     pathname.startsWith("/api") ||
-    pathname.startsWith("/favicon.ico")
+    pathname.startsWith("/favicon.ico") ||
+    pathname.startsWith("/images") ||
+    pathname.match(/\.(.*)$/)
   ) {
     return NextResponse.next();
   }
 
-  if (!token) {
-    if (pathname !== "/login") {
-      return NextResponse.redirect(new URL("/login", req.url));
-    }
+  if (!token && (pathname === "/" || pathname === "/login")) {
     return NextResponse.next();
   }
 
-  if (token) {
-    if (pathname === "/login") {
-      return NextResponse.redirect(new URL("/admin", req.url));
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
-    return NextResponse.next();
+  if (token && (pathname === "/login" || pathname === "/")) {
+    return NextResponse.redirect(new URL("/admin", req.url));
   }
 
   return NextResponse.next();
