78174ae4dbfe1fc3fa4f10379adfd61c0062a971
Praneeth <iampraneeth1116@gmail.com>
2025-11-20 12:57:39 +0530

    fixed bugs

--- prisma/migrations/20251120072651/migration.sql
+++ prisma/migrations/20251120072651/migration.sql
@@ -24,7 +24,6 @@ CREATE TABLE `UserLogin` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-
 CREATE TABLE `Student` (
     `id` VARCHAR(191) NOT NULL,
     `username` VARCHAR(191) NOT NULL,
