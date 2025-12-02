7fc191ebebccd091dd15a01bfb8290f5f676ede9
Praneeth <iampraneeth1116@gmail.com>
2025-12-02 17:43:58 +0530

    updated tablesearch and usercard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,42 +1,46 @@
-import UserCard from "@/components/UserCard";
-// import CountChart from "@/components/CountChart";
+import Announcements from "@/components/Announcements";
 import AttendanceChartContainer from "@/components/AttendanceChartContainer";
-// import FinanceChart from "@/components/FinanceChart";
-// import EventCalendar from "@/components/EventCalendar";
-// import Announcements from "@/components/Announcements";
+import CountChartContainer from "@/components/CountChartContainer";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
 
-const AdminPage = () => {
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* L */}
+
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
 
         <div className="flex gap-4 justify-between flex-wrap">
-          <UserCard type="student" />
+          <UserCard type="admin" />
           <UserCard type="teacher" />
+          <UserCard type="student" />
           <UserCard type="parent" />
-          <UserCard type="staff" />
         </div>
-        {/* MIDDLE CHARTS */}
+ 
         <div className="flex gap-4 flex-col lg:flex-row">
 
           <div className="w-full lg:w-1/3 h-[450px]">
-            {/* <CountChart /> */}
+            <CountChartContainer />
           </div>
 
           <div className="w-full lg:w-2/3 h-[450px]">
             <AttendanceChartContainer />
           </div>
         </div>
-        {/* BOTTOM CHART */}
+
         <div className="w-full h-[500px]">
-          {/* <FinanceChart /> */}
+          <FinanceChart />
         </div>
       </div>
-      {/* R */}
+      {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {/* <EventCalendar /> */}
-        {/* <Announcements/> */}
+        <EventCalendarContainer searchParams={searchParams}/>
+        <Announcements />
       </div>
     </div>
   );
