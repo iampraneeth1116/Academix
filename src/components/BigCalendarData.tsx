a720bc438bf58e15b91e6a1b262760cfcc8c408f
Praneeth <iampraneeth1116@gmail.com>
2025-12-02 16:24:07 +0530

    added bigcalendardata

---
+++ src/components/BigCalendarData.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Calendar, momentLocalizer, View } from "react-big-calendar";
+import moment from "moment";
+import { useState } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+export type CalendarEvent = {
+  title: string;
+  start: Date;
+  end: Date;
+};
+
+const BigCalendar = ({ data }: { data: CalendarEvent[] }) => {
+  const [view, setView] = useState<View>("work_week");
+
+  return (
+    <div className="h-[90vh] w-full p-4">
+      <Calendar
+        localizer={localizer}
+        events={data}
+        startAccessor="start"
+        endAccessor="end"
+        view={view}
+        views={["work_week", "day", "month", "week"]}
+        onView={(v) => setView(v)}
+        min={new Date(2025, 0, 1, 8, 0)}
+        max={new Date(2025, 0, 1, 17, 0)}
+        style={{ height: "100%", backgroundColor: "white", borderRadius: 12 }}
+      />
+    </div>
+  );
+};
+
+export default BigCalendar;
